753f7a8f1b0d6773f1af4182de00ec0e95178757
Kasper Chen <kasperchen@KasperdeMacBook-Pro.local>
2023-02-05 21:04:00 +0800

    add gitsigns config

--- lua/Kai-Po/plugins/gitsigns.lua
+++ lua/Kai-Po/plugins/gitsigns.lua
@@ -4,5 +4,18 @@ if not setup then
 	return
 end
 
+local keymap = vim.keymap
+
+local on_attach = function(client, bufnr)
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+	-- keymaps --
+	keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+	keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
+	keymap.set("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
+end
+
 -- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup({
+	current_line_blame = true,
+	on_attach = on_attach,
+})
