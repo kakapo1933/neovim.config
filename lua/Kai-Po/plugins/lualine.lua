7e9a58de12e18066732d4322c65ecfb2f7b64f4d
Kasper Chen <kasperchen@KasperdeMacBook-Pro.local>
2023-02-17 17:23:14 +0800

    fix sumneko issue

--- lua/Kai-Po/plugins/lualine.lua
+++ lua/Kai-Po/plugins/lualine.lua
@@ -1,32 +1,32 @@
 local status, lualine = pcall(require, "lualine")
 if not status then
-  return
+	return
 end
 
 local lualine_nightfly = require("lualine.themes.nightfly")
+local lualine_dracula = require("lualine.themes.dracula")
 
 local new_colors = {
-  blue = "#65D1FF",
-  green = "#3EFFDC",
-  violet = "#FF61EF",
-  yellow = "#FFDA7B",
-  black = "#000000",
+	blue = "#8BE9FD",
+	green = "#50FA7B",
+	violet = "#BD93F9",
+	yellow = "#F1FA8C",
+	black = "#000000",
 }
 
 lualine_nightfly.normal.a.bg = new_colors.blue
 lualine_nightfly.insert.a.bg = new_colors.green
 lualine_nightfly.visual.a.bg = new_colors.violet
 lualine_nightfly.command = {
-  a = {
-    gui = "bold",
-    bg = new_colors.yellow,
-    fg = new_colors.black,
-  },
+	a = {
+		gui = "bold",
+		bg = new_colors.yellow,
+		fg = new_colors.black,
+	},
 }
 
 lualine.setup({
-  options = {
-    theme = lualine_nightfly
-  }
+	options = {
+		theme = lualine_dracula,
+	},
 })
-
